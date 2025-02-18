787455e4ad51bf77159ff76db1bba2603b35f4ba
DBobur <davlatboyev2024@gmail.com>
2025-02-18 19:00:04 +0500

    test

--- src/main/java/creational/singleton/Main.java
+++ src/main/java/creational/singleton/Main.java
@@ -4,7 +4,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 
 public class Main {
-    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+    public static void main(String[] args){
         //Eager Initialization Singleton
         //Bill Pugh Singleton
         //Lazy Initialization Singleton
@@ -15,23 +15,45 @@ public class Main {
 
         System.out.println("Reflection test");
 
-        Constructor<ConfigManager> constructor = ConfigManager.class.getDeclaredConstructor();
-        constructor.setAccessible(true);
-        ConfigManager newInstance = constructor.newInstance();
-        ConfigManager newInstance1 = constructor.newInstance();
-        System.out.println(newInstance.hashCode());
-        System.out.println(newInstance1.hashCode());
-        System.out.println(configManager.hashCode());
+        Constructor<ConfigManager> constructor = null;
+        try {
+            constructor = ConfigManager.class.getDeclaredConstructor();
+            constructor.setAccessible(true);
+            ConfigManager newInstance = constructor.newInstance();
+            ConfigManager newInstance1 = constructor.newInstance();
+            System.out.println(newInstance.hashCode());
+            System.out.println(newInstance1.hashCode());
+            System.out.println(configManager.hashCode());
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
 
         System.out.println("Reflection test2");
         MySingleton mySingleton = MySingleton.getInstance();
         System.out.println(mySingleton.hashCode());
-        Constructor<MySingleton> constructor1 = MySingleton.class.getDeclaredConstructor();
-        constructor1.setAccessible(true);
-        MySingleton newInstance2 = constructor1.newInstance();
-        System.out.println(newInstance2.hashCode());
-        MySingleton newInstance3 = constructor1.newInstance();
-        System.out.println(newInstance3.hashCode());
+        Constructor<MySingleton> constructor1 = null;
+        try {
+            constructor1 = MySingleton.class.getDeclaredConstructor();
+            constructor1.setAccessible(true);
+            MySingleton newInstance2 = constructor1.newInstance();
+            System.out.println(newInstance2.hashCode());
+            MySingleton newInstance3 = constructor1.newInstance();
+            System.out.println(newInstance3.hashCode());
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (InstantiationException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        }
 
 
     }
--- src/main/java/creational/singleton/MySingleton.java
+++ src/main/java/creational/singleton/MySingleton.java
@@ -3,6 +3,7 @@ package creational.singleton;
 public class MySingleton {
     private MySingleton() {
         if(INSTANCE != null){
+            System.out.println("MySingleton object is already instantiated");
             throw new RuntimeException("Can't instantiate MySingleton");
         }
     }
